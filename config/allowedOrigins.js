972f3febc76cf6807e24a1cacec1601a53a09f84
HoangDang <dang.hoangthe2209@gmail.com>
2024-08-10 22:55:29 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    "https://fe-english-web.vercel.app/3000"
 ];
 
 module.exports = allowedOrigins;
